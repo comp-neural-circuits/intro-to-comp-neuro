47fea711df8c64129d9c037bd5ecf6e9b05c9996
Sebastian Onasch <sebastian.onasch@posteo.de>
2022-07-12 10:48:24 +0200

    add solution

---
+++ notebooks/Exc_1/solutions/solution_1.py
@@ -0,0 +1,10 @@
+# Define the first variable and assign the value 7
+first_var = 7
+# Now define the second value and assign the value 5
+second_var = 5
+
+# Now you can define a third variable and assign the sum of the two values to it
+final_var = first_var + second_var
+
+# And finally we print the number
+print (final_var)
