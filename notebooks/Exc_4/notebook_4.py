70629a6b869820446730a9a9964d3d8c56eda32c
Sebastian Onasch <sebastian.onasch@posteo.de>
2022-11-15 09:14:49 +0100

    intermediate stage

--- notebooks/Exc_4/notebook_4.py
+++ notebooks/Exc_4/notebook_4.py
@@ -15,13 +15,16 @@
 
 # # Neural encoding 1
 
-TODO - include figure here -> spikes - neuron - LIF - spikes
+# TODO - include figure here -> spikes - neuron - LIF - spikes
 
 # +
 import io
 import requests
 import numpy as np
 import matplotlib.pyplot as plt
+import ipywidgets as widgets
+
+# %matplotlib inline
 
 # Settings for the figures
 plt.style.use(plt.style.available[20])
@@ -45,8 +48,184 @@ if r.status_code != 200:
 else:
   spike_times = np.load(io.BytesIO(r.content), allow_pickle=True)['spike_times']
 
-type(spike_times)
+# +
+spike_times = np.load(io.BytesIO(r.content), allow_pickle=True)
+
+for key in spike_times: 
+    print (key)
+# -
 
 
 
+fig, ax = plt.subplots()
+ax.eventplot(spike_times['spike_times'][:40], color='#525252')
+ax.set(xlim = (0,500))
 
+# +
+# https://github.com/nsteinme/steinmetz-et-al-2019/wiki/data-files
+
+# @title Data retrieval
+import os, requests
+
+fname = []
+for j in range(3):
+  fname.append('steinmetz_part%d.npz'%j)
+url = ["https://osf.io/agvxh/download"]
+url.append("https://osf.io/uv3mw/download")
+url.append("https://osf.io/ehmw2/download")
+
+for j in range(len(url)):
+  if not os.path.isfile(fname[j]):
+    try:
+      r = requests.get(url[j])
+    except requests.ConnectionError:
+      print("!!! Failed to download data !!!")
+    else:
+      if r.status_code != requests.codes.ok:
+        print("!!! Failed to download data !!!")
+      else:
+        with open(fname[j], "wb") as fid:
+          fid.write(r.content)
+# -
+
+# @title Data loading
+alldat = np.array([])
+for j in range(len(fname)):
+  alldat = np.hstack((alldat,
+                      np.load('steinmetz_part%d.npz'%j,
+                              allow_pickle=True)['dat']))
+
+# +
+dat = alldat[1]
+# for key in dat['spks']:
+#     print (len(key))
+
+# for key in dat:
+#     print (key)
+
+# neurons, trials, time_bins
+print (dat['spks'].shape)
+
+
+single_trial = dat['spks'][0,0,:]
+
+def transform_to_event_input(binned_spikes):
+    bin_size = 10
+    bin_offset = 5
+    n_bins = len(binned_spikes)
+    bin_times = np.linspace(bin_offset,
+                            n_bins*bin_size-(bin_size-bin_offset),
+                            n_bins)
+    spike_times = binned_spikes * bin_times
+    spike_times = spike_times[spike_times != 0]
+    return bin_times, spike_times
+
+    
+    
+fig, ax = plt.subplots()
+
+for ii in range(0,60):
+    _, spike_times = transform_to_event_input(dat['spks'][14,ii,:])
+    ax.eventplot(spike_times, lineoffsets=ii)
+
+ax.set(
+    xlim = (0,2500)
+)
+
+
+# -
+
+# $w(\tau) = \left[\alpha^2 \tau \exp(-\alpha \tau)\right]_+$
+#
+# [more info on spikes to rates](https://www.youtube.com/watch?v=Ef7_qnLOh70)
+
+# +
+
+def visualize_filtering(pos, filter_shape = 'rectangle', trial_number = 0):
+    
+    if filter_shape == 'large rectangle':
+        filter_array = np.ones(25)
+        
+    if filter_shape == 'small rectangle':
+        filter_array = np.ones(11)
+    
+    if filter_shape == 'gaussian, sigma = 25':
+        def gaussian(x, mu, sig):
+            return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
+        
+        filter_array = gaussian(np.linspace(0,159,159), 79.5, 25)
+
+        
+    if filter_shape == 'gaussian, sigma = 8':
+        def gaussian(x, mu, sig):
+            return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
+        
+        filter_array = gaussian(np.linspace(0,63,63), 31.5, 8)
+    
+    
+    if filter_shape == 'causal':
+        def causal(alpha, x):
+            result = alpha**2*x*np.exp(-alpha*x)
+            result[result <0] = 0
+            return result
+        filter_array = causal(0.2,np.linspace(0,63,63))
+        filter_array /= np.max(filter_array)
+            
+            
+        
+        
+    single_trial = dat['spks'][0,trial_number,:]
+    
+    
+    shift_ = 80
+    shifted_trial = np.hstack([np.zeros(shift_),single_trial, np.zeros(shift_)])
+    
+    bin_times, spike_times = transform_to_event_input(single_trial)
+            
+    
+    fig,axes = plt.subplots(2)
+    axes[0].eventplot(spike_times, label = 'spikes')
+    axes[0].set(
+    xlim = (0,2500),
+    ylim = (-0.1,2.8))
+
+    filter_backbone = np.zeros_like(shifted_trial)
+    new_bin_times = np.hstack([np.zeros(shift_)*float('nan'),bin_times,np.zeros(shift_)*float('nan')])
+    
+    shifted_pos = pos + shift_
+    filter_backbone[shifted_pos -len(filter_array)//2:shifted_pos +len(filter_array)//2+1] = filter_array
+    axes[0].plot(new_bin_times, filter_backbone, color='#feb24c', label='filter')
+    axes[0].plot([new_bin_times[shifted_pos],new_bin_times[shifted_pos]],[0,0.2],color='#de2d26', label='time point')
+    axes[0].plot([new_bin_times[shift_+2],new_bin_times[shift_+27]],[0.2,0.2],color='k')
+    axes[0].annotate('250 ms', xy=([new_bin_times[shift_+2], 0.25]),  xycoords='data',
+            xytext=([new_bin_times[shift_+5], 0.25]), textcoords='data')
+    
+    axes[0].axis('off')
+    axes[0].legend()
+    
+    filtered_signal = np.zeros_like(shifted_trial)
+    for ii in range(shift_,shifted_pos):
+        filtered = 100*len(filter_array) * np.mean(shifted_trial[ii-len(filter_array)//2:ii+len(filter_array)//2+1] * filter_array/np.sum(filter_array))
+        filtered_signal[ii] = filtered
+
+    
+    axes[1].plot(new_bin_times, filtered_signal, label= 'rate')
+    axes[1].legend()
+    axes[1].set(
+        xlim=(0,2500),
+        xlabel = 'Time in ms',
+        ylabel = 'Firing rate in Hz',
+    )
+
+
+widgets.interactive(visualize_filtering, 
+                    pos = (0,250,1), 
+                    filter_shape = ['large rectangle','small rectangle','gaussian, sigma = 8','gaussian, sigma = 25','causal'],
+                   trial_number = (0,100,1))
+# -
+
+# #  Do the same but for averaging across trials
+#
+# this is essentially another interpretation
+
+single_trial = dat['spks'][0,trial_number,:]
--- notebooks/Exc_5/notebook_5.py
+++ notebooks/Exc_5/notebook_5.py
@@ -6,7 +6,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.14.0
+#       jupytext_version: 1.14.1
 #   kernelspec:
 #     display_name: intro-to-comp-neuro
 #     language: python
@@ -14,5 +14,74 @@
 # ---
 
 import numpy as np
+import ipywidgets as widgets
+
+
+# +
+def input_signal(rate_max_Hz=100, ipd_deg=90, f_Hz=3):
+    # We can't use values with units in the widgets, so we add the units first
+    rate_max = rate_max_Hz
+    ipd = (pi/180)*ipd_deg
+    f = f_Hz
+    
+    # These are the equations governing the ear neurons. Take a look at the
+    # Brian documentation for an explanation, but the only thing you might
+    # find non-obvious hopefully is the ": 1" and ": Hz" which tells Brian
+    # what the units of the variable being defined are (1 means dimensionless).
+    # Also note that the variable "i" is defined in Brian to be the index of
+    # the neuron, so for neuron 0 it will be 0 and for neuron 1 it will be 1,
+    # allowing us to make the input signal different for the two ears.
+    eqs_ears = '''
+    theta = 2*pi*f*t + i*ipd : 1
+    rate = rate_max*0.5*(1+sin(theta)) : Hz
+    '''
+    
+    # Create a group of 2 neurons with these equations, that fires a spike
+    # according to a Poisson process with the given time-varying rate. We
+    # use a dt of 1ms to speed up the simulation for interactivity, but later
+    # we'll use a better default of 0.1ms.
+    ears = NeuronGroup(2, eqs_ears, threshold='rand()<rate*dt', dt=1*ms)
+    
+    # Record the spikes and values of the rate as we run the simulation
+    M_spike = SpikeMonitor(ears)
+    M_state = StateMonitor(ears, 'rate', record=True)
+    
+    # Run the simulation for 1 second
+    run(1*second)
+
+    # Now plot the results. I won't explain in detail because it's mostly
+    # just fiddly matplotlib stuff to make it look nice.
+    trains = M_spike.spike_trains()
+    fig = figure(figsize=(4, 2), dpi=200)
+    gs = gridspec.GridSpec(2, 1, hspace=0, height_ratios=[1, .3])
+    ax = subplot(gs[0])
+    plot(M_state.t/ms, M_state.rate[0]/Hz, label='Left ear')
+    plot(M_state.t/ms, M_state.rate[1]/Hz, label='Right ear')
+    legend(loc='upper right')
+    gca().set_frame_on(False)
+    ylabel('Rate')
+    yticks([])
+    xticks([])
+    ylim(-10, 210)
+    subplot(gs[1], sharex=ax)
+    plot(trains[0]/ms, [0]*len(trains[0]), '|')
+    plot(trains[1]/ms, [1]*len(trains[1]), '|')
+    ylim(-1, 2)
+    gca().set_frame_on(False)
+    xlabel('Time')
+    ylabel('Spikes')
+    yticks([])
+    xticks([])
+    tight_layout()
+
+if widgets is not None:
+    widgets.interact(input_signal,
+        rate_max_Hz=widgets.IntSlider(min=10, max=200, value=100, step=10, continuous_update=False),
+        ipd_deg=widgets.IntSlider(min=0, max=360, value=90, step=10, continuous_update=False),
+        f_Hz=widgets.FloatSlider(min=0, max=10, value=3, step=.1, continuous_update=False),
+        );
+else:
+    input_signal()
+# -
 
 
