2eafe305bd95b324bafad2a215663c0083a91741
Sebastian Onasch <sebastian.onasch@posteo.de>
2022-12-22 15:14:54 +0100

    try html5 at colab

--- notebooks/Exc_10/notebook_10.py
+++ notebooks/Exc_10/notebook_10.py
@@ -31,6 +31,7 @@ import gif
 # Settings for the figures
 plt.style.use(plt.style.available[20])
 plt.style.use("https://github.com/comp-neural-circuits/intro-to-comp-neuro/raw/dev/plots_style.txt")
+plt.rcParams["animation.html"] = "jshtml"
 
 # +
 np.random.seed(12)
@@ -93,8 +94,6 @@ def animate(i):
         line_plots[jj].set_xdata([xx,projection[0]])
         scatter_plots[jj].set_offsets(projection)
         
-        
-        line_plots.append(dummy)
     
     return line_plots
 
@@ -104,17 +103,29 @@ def init():
     return line_plot,
 
 
-ani = FuncAnimation(fig, animate, np.linspace(0, 179,360), init_func=init,
-                              interval=25, blit=True)
-ani.save('animation.gif', writer='Pillow', fps=24)
+ani = FuncAnimation(fig, animate, frames=np.linspace(0, 179,360), init_func=init,
+                              interval=20, blit=False)
+# ani.save('animation.gif', writer='Pillow', fps=24)
+from IPython.display import HTML
+HTML(ani.to_jshtml(fps=20,embed_frames=False))
 
 # +
 # project data into a new space
 
+np.random.seed(12)
 
-vec = np.array([1,1])
+X = -0.9 + np.random.rand(45)*1.8
+Y = 0.42*X + (-0.5 + np.random.rand(45))
+# vec = np.array([[1,0.25],[0.5,1],[1,1]]).T
+vec = np.vstack([X,Y])
+
+print (vec.shape)
+
+
+
 
 def interact(i):
+    
     fig, (ax, new_ax) = plt.subplots(1,2, figsize=(12,6))
     ax.scatter(*vec, c = '#1f78b4', s=50)
     ax.set_box_aspect(1)
@@ -123,10 +134,9 @@ def interact(i):
 #     i = 120 # in degree
 
 
-
+    # calculate
     alpha = 2*np.pi*i/360.
     a = np.tan(alpha)
-    line_plot.set_ydata(a*x_line)
     
     
     x_0 = 1
@@ -140,35 +150,31 @@ def interact(i):
 
     norm_x = vec_x/np.sqrt(np.sum(vec_x**2))
     norm_y = vec_y/np.sqrt(np.sum(vec_y**2))
-
-    print (*norm_x)
-    
     
     project_x = np.dot(norm_x, vec)
     project_y = np.dot(norm_y, vec)
-
-    # plot vectors of new coordinate system
+      
+                       
+    # PLOT onto axes
     ax.quiver(0,0,*norm_x, scale = 2, color='#984ea3')
     ax.quiver(0,0,*norm_x*-1, scale = 2, color='#984ea3', headlength=0, headaxislength=0)
     ax.quiver(0,0,*norm_y, scale = 2, color='#ff7f00')
     ax.quiver(0,0,*norm_y*-1, scale = 2, color='#ff7f00', headlength=0, headaxislength=0)
     
-    ax.scatter(*(norm_x * project_x), color = '#984ea3')
-    ax.scatter(*(norm_y * project_y), color = '#ff7f00')
+    for bb, cc in zip([(norm_x * np.vstack([project_x,project_x]).T),(norm_y * np.vstack([project_y,project_y]).T)],( '#984ea3','#ff7f00')):
+        
+        ax.scatter(*bb.T, color = cc) 
+        for a,b in zip(bb,vec.T):
+            ax.plot([a[0],b[0]],[a[1],b[1]], linestyle = '--', color = cc)
+            
+                       
+                       
+    new_ax.scatter([project_x],[project_y], c = '#1f78b4', s=50)
 
 
-    ax.set_xlabel('$x_1$', fontsize=25, fontweight='bold')
-    ax.set_ylabel('$x_2$', fontsize=25, rotation=0, fontweight='bold', labelpad=19)
     
-    
-    x_ticks = [-1,0,1]
-    y_ticks = [-1,0,1]
-    ax.set_xticks(x_ticks)
-    ax.set_xticklabels(x_ticks,fontsize=20)
-    ax.set_yticks(y_ticks)
-    ax.set_yticklabels(y_ticks,fontsize=20)
-    
-    
+        
+    # Style of the axes
     for dummy_ax in [ax,new_ax]:
         
         dummy_ax.set(
@@ -186,15 +192,179 @@ def interact(i):
         dummy_ax.set_yticks(y_ticks)
         dummy_ax.set_yticklabels(y_ticks,fontsize=20)
         
-        
+    ax.set_xlabel('$x_1$', fontsize=25, fontweight='bold')
+    ax.set_ylabel('$x_2$', fontsize=25, rotation=0, fontweight='bold', labelpad=19)
+    
+    new_ax.set_xlabel('$c_1$', fontsize=25, fontweight='bold')
+    new_ax.set_ylabel('$c_2$', fontsize=25, rotation=0, fontweight='bold', labelpad=19)
+    
     new_ax.spines['left'].set_color('#ff7f00')
-    new_ax.spines['bottom'].set_color('#984ea3')
-    
-    new_ax.scatter([project_x],[project_y], c = '#1f78b4', s=50)
+    new_ax.spines['bottom'].set_color('#984ea3')    
     
     
-    
+    plt.tight_layout()
 widgets.interactive(interact, i=(0,359,1))
+
+
+# -
+
+def calculations(i, vec):
+    # calculations
+    alpha = 2*np.pi*i/360.
+    a = np.tan(alpha)
+
+    x_0 = 1
+    if 90 < i <= 270:
+        x_0 = -1
+        a = -a
+
+
+    vec_x = np.array([x_0,a])
+    vec_y = np.array([-a,x_0])
+
+    norm_x = vec_x/np.sqrt(np.sum(vec_x**2))
+    norm_y = vec_y/np.sqrt(np.sum(vec_y**2))
+
+    project_x = np.dot(norm_x, vec)
+    project_y = np.dot(norm_y, vec)
+    
+    
+    return vec_x, vec_y, norm_x, norm_y, project_x, project_y
+
+# +
+# project data into a new space
+show_projection_line_plots = True
+
+
+
+np.random.seed(12)
+
+
+# create vector
+X = -0.9 + np.random.rand(45)*1.8
+Y = 0.42*X + (-0.5 + np.random.rand(45))
+vec = np.vstack([X,Y])
+
+# vec = np.array([[1,0.25],[0.5,1],[1,1]]).T
+
+
+
+
+
+
+
+
+# calculations
+vec_x, vec_y, norm_x, norm_y, project_x, project_y = calculations(i=0, vec=vec)
+
+
+# create figure
+fig, (ax, new_ax) = plt.subplots(1,2, figsize=(12,6))
+
+
+# PLOT onto axes
+qiver_x_1 = ax.quiver(0,0,*norm_x, scale = 2, color='#984ea3')
+qiver_x_2 = ax.quiver(0,0,*norm_x*-1, scale = 2, color='#984ea3', headlength=0, headaxislength=0)
+qiver_y_1 = ax.quiver(0,0,*norm_y, scale = 2, color='#ff7f00')
+qiver_y_2 = ax.quiver(0,0,*norm_y*-1, scale = 2, color='#ff7f00', headlength=0, headaxislength=0)
+
+
+projection_scatter_plots = []
+projection_line_plots = []
+for bb, cc in zip([(norm_x * np.vstack([project_x,project_x]).T),(norm_y * np.vstack([project_y,project_y]).T)],( '#984ea3','#ff7f00')):
+
+    projection_scatter_plots.append(ax.scatter(*bb.T, color = cc)) 
+    for a,b in zip(bb,vec.T):
+        line_plot, = ax.plot([a[0],b[0]],[a[1],b[1]], linestyle = '--', color = cc) # the , is important
+        projection_line_plots.append(line_plot)
+        
+ax.scatter(*vec, c = '#1f78b4', s=50) # this remains constant
+
+
+
+new_ax_scatter_plot = new_ax.scatter([project_x],[project_y], c = '#1f78b4', s=50)
+
+
+
+
+# Style of the axes
+for dummy_ax in [ax,new_ax]:
+    dummy_ax.set_box_aspect(1)
+    dummy_ax.set(
+        xlim = [-1.5,1.5],
+        ylim = [-1.5,1.5])
+
+    for axis in ['top','bottom','left','right']:
+        dummy_ax.spines[axis].set_linewidth(2)
+    dummy_ax.tick_params(width=2)
+
+    x_ticks = [-1,0,1]
+    y_ticks = [-1,0,1]
+    dummy_ax.set_xticks(x_ticks)
+    dummy_ax.set_xticklabels(x_ticks,fontsize=20)
+    dummy_ax.set_yticks(y_ticks)
+    dummy_ax.set_yticklabels(y_ticks,fontsize=20)
+
+ax.set_xlabel('$x_1$', fontsize=25, fontweight='bold')
+ax.set_ylabel('$x_2$', fontsize=25, rotation=0, fontweight='bold', labelpad=19)
+
+new_ax.set_xlabel('$c_1$', fontsize=25, fontweight='bold')
+new_ax.set_ylabel('$c_2$', fontsize=25, rotation=0, fontweight='bold', labelpad=19)
+
+new_ax.spines['left'].set_color('#ff7f00')
+new_ax.spines['bottom'].set_color('#984ea3')    
+plt.tight_layout()
+
+
+
+def animate(i):
+    
+    
+    vec_x, vec_y, norm_x, norm_y, project_x, project_y = calculations(i, vec=vec)
+    
+    
+#     # PLOT onto axes
+#     qiver_x_1 = ax.quiver(0,0,*norm_x, scale = 2, color='#984ea3')
+#     qiver_x_2 = ax.quiver(0,0,*norm_x*-1, scale = 2, color='#984ea3', headlength=0, headaxislength=0)
+#     qiver_y_1 = ax.quiver(0,0,*norm_y, scale = 2, color='#ff7f00')
+#     qiver_y_2 = ax.quiver(0,0,*norm_y*-1, scale = 2, color='#ff7f00', headlength=0, headaxislength=0)
+
+
+    
+    for ii, (bb, cc) in enumerate(zip([(norm_x * np.vstack([project_x,project_x]).T),(norm_y * np.vstack([project_y,project_y]).T)],( '#984ea3','#ff7f00'))):
+
+        projection_scatter_plots[ii].set_offsets(bb.T) 
+#         for a,b in zip(bb,vec.T):
+#             line_plot, = ax.plot([a[0],b[0]],[a[1],b[1]], linestyle = '--', color = cc) # the , is important
+#             projection_line_plots.append(line_plot)
+#     new_ax_scatter_plot = new_ax.scatter([project_x],[project_y], c = '#1f78b4', s=50)
+    
+    
+    
+#     for jj, (xx,yy) in enumerate(zip(X,Y)):
+        
+#         projection = np.array([xx,yy])@v.T/(v.T@v)*v
+#         line_plots[jj].set_ydata([yy,projection[1]])
+#         line_plots[jj].set_xdata([xx,projection[0]])
+        
+        
+#         new_ax_scatter_plot.set_offsets(projection)
+        
+        
+#         line_plots.append(dummy)
+    
+    return projection_scatter_plots
+
+# Init only required for blitting to give a clean slate.
+# def init():
+#     line_plot.set_ydata(np.ma.array(x_line, mask=True))
+#     return line_plot,
+
+
+ani = FuncAnimation(fig, animate, np.linspace(0, 20,21), init_func=init,
+                              interval=25, blit=True)
+# ani.save('rotate_coordinate_system.gif', writer='Pillow', fps=24)
+plt.show()
 
 # +
 import matplotlib.pyplot as plt
